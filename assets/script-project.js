6edabedde594513310ab59eeefd740db8805d18f
carriewdesign <cwlearningdesign@gmail.com>
2024-12-28 17:05:06 -0500

    carriewdesign

--- assets/script-project.js
+++ assets/script-project.js
@@ -13,3 +13,43 @@ function closeModal() {
     const modal = document.getElementById("image-modal");
     modal.style.display = "none"; // Hide modal
 }
+
+
+
+
+// Get the button element
+const goTopBtn = document.getElementById("goTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        goTopBtn.style.display = "block"; // Show button
+    } else {
+        goTopBtn.style.display = "none"; // Hide button
+    }
+};
+
+// When the user clicks the button, scroll to the top of the document
+goTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+
+
+/* --- scroll debug
+// Show or hide the button based on scroll position
+window.addEventListener("scroll", function() {
+    const button = document.getElementById("goTopBtn");
+
+    // Print scroll positions for debugging
+    console.log("scrollY:", window.scrollY); // You can use document.documentElement.scrollTop as well
+
+    // Set threshold to 300px for the button to appear
+    if (window.scrollY > 300) {
+        button.style.display = "block"; // Show button when scrolled past 300px
+    } else {
+        button.style.display = "none"; // Hide button when above 300px
+    }
+});
+
+*/
--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+
+// Get the button element
+const goTopBtn = document.getElementById("goTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        goTopBtn.style.display = "block"; // Show button
+    } else {
+        goTopBtn.style.display = "none"; // Hide button
+    }
+};
+
+// When the user clicks the button, scroll to the top of the document
+goTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
